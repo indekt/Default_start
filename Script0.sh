de0e2c504fb4453bfb32f2bd80cdc9d3b4b6992c
indekt <70900981+indekt@users.noreply.github.com>
2021-04-21 17:17:23 +0200

    Add files via upload

---
+++ Script0.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+if ! hash java 2>/dev/null; then
+    sudo apt install default-jdk -y && sudo apt upgrade && sudo apt clean
+fi
+if ! hash git 2>/dev/null; then
+    sudo apt install git -y && sudo apt upgrade && sudo apt clean
+fi
+if ! hash screen 2>/dev/null; then
+    sudo apt install screen -y && sudo apt upgrade && sudo apt clean
+fi
+sudo git clone https://github.com/indekt/Default_start.git
